6c2d6c421799b659250c13002fccf0e470482bd0
Rodrigo-Santos-Dias <digoh.a4@gmail.com>
2024-07-15 16:54:43 -0300

    Criação da repository Usuario

---
+++ src/main/java/com/blog/blogpessoal/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.blog.blogpessoal.repository;
+
+import com.blog.blogpessoal.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    public Optional<Usuario> findByUsuario(String usuario);
+}
